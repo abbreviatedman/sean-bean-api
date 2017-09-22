b330c84519f125166fda633bf43602099f63ca54
Colin Jaffe <balloonasaurus@gmail.com>
2017-09-22 19:25:23 -0400

    rewinding

--- server.js
+++ server.js
@@ -1,79 +1,14 @@
-// import express from 'express';
-// import morgan from 'morgan';
-// import cloudinary from 'cloudinary';
-// import bodyParser from 'body-parser';
-// import mongodb from 'mongodb';
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-// const {ObjectID, MongoClient} = mongodb;
-// const app = express();
+var MOVIES_COLLECTION = "movies";
 
-// const MOVIE_COLLECTION = "movies";
-
-// cloudinary.config({ 
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-//   api_key: process.env.CLOUDINARY_API_KEY, 
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// });
-
-// app.use(bodyParser.json());
-
-// let db;
-// MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-//   if (err) {
-//     console.log(err);
-//     process.exit(1);
-//   }
-  
-//   db = database;
-//   console.log("Database connection ready");
-
-//   const server = app.listen(process.env.PORT || 8080, function () {
-//     const port = server.address().port;
-//     console.log("App now running on port", port);
-//   });
-// });
-
-// const handleError = (res, reason, message, code) => {
-//   console.error(`ERROR: ${reason}`);
-//   res.status(code || 500).json({'error': message});
-// };
-
-// app.get('/api/movies', (req, res) => {
-//   db.collection(MOVIE_COLLECTION).find({}).toArray((err, documents) => {
-//     if (err) {
-//       handleError(res, err.message, "Failed to get movie data from database.");
-//     } else {
-//       res.status(200, json(documents))
-//     }
-//   });
-// });
-
-// app.post("/api/movies", function(req, res) {
-//   var newMovie = req.body;
-
-//   db.collection(MOVIE_COLLECTION).insertOne(newMovie, function(err, doc) {
-//     if (err) {
-//       handleError(res, err.message, "Failed to add new movie to database.");
-//     } else {
-//       res.status(201).json(doc.ops[0]);
-//     }
-//   });
-// });
-
-
-// {"title": "The Field", "year": 1990, "cloudinaryName": "thefield", "dies": true}
-// curl -H "Content-Type: application/json" -d "{'title': 'The Field', 'year': 1990, 'cloudinaryName': 'thefield', 'dies': true}" http://seanbeanapi.herokuapp.com/api/movies
-
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongodb from 'mongodb';
-
-const MOVIES_COLLECTION = "movies";
-
-const app = express();
+var app = express();
 app.use(bodyParser.json());
 
-let db;
+var db;
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
@@ -81,20 +16,29 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
     process.exit(1);
   }
 
+  // Save database object from the callback for reuse.
   db = database;
   console.log("Database connection ready");
 
-  const server = app.listen(process.env.PORT || 8080, function () {
-    const port = server.address().port;
-    console.log('App now running on port: ' + port);
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
   });
 });
 
+// movies API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log('ERROR: ' + reason);
+  console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
 
+/*  "/api/movies"
+ *    GET: finds all movies
+ *    POST: creates a new contact
+ */
 
 app.get("/api/movies", function(req, res) {
   db.collection(MOVIES_COLLECTION).find({}).toArray(function(err, docs) {
@@ -107,7 +51,7 @@ app.get("/api/movies", function(req, res) {
 });
 
 app.post("/api/movies", function(req, res) {
-  const newMovie = req.body;
+  var newMovie = req.body;
 
   db.collection(MOVIES_COLLECTION).insertOne(newMovie, function(err, doc) {
     if (err) {
@@ -117,3 +61,7 @@ app.post("/api/movies", function(req, res) {
     }
   });
 });
+
+
+
+// curl -H "Content-Type: application/json" -d '{"name":"mLab Support", "email": "support@mlab.com"}' http://anothermlabtest.herokuapp.com/api/movies
