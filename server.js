2b676deed53051d04037dc109ee2f3d240485a5f
Colin Jaffe <balloonasaurus@gmail.com>
2017-09-22 18:31:10 -0400

    stripped api endpoints down

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-// require('dotenv').config();
 // import express from 'express';
 // import morgan from 'morgan';
-// import mongoose from 'mongoose';
 // import cloudinary from 'cloudinary';
+// import bodyParser from 'body-parser';
+// import mongodb from 'mongodb';
 
-// import router from './router';
-
+// const {ObjectID, MongoClient} = mongodb;
 // const app = express();
-// const logger = morgan('combined');
-// const port = process.env.PORT || 3000;
+
+// const MOVIE_COLLECTION = "movies";
 
 // cloudinary.config({ 
 //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -16,78 +15,105 @@
 //   api_secret: process.env.CLOUDINARY_API_SECRET
 // });
 
-// mongoose.connect('mongodb://localhost/movies');
+// app.use(bodyParser.json());
 
-// app.use(logger);
-// app.use('/v1', router);
+// let db;
+// MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+  
+//   db = database;
+//   console.log("Database connection ready");
 
-// app.get('/', (req, res) => {
-//   const url = cloudinary.url('thefield');
-//   res.send(url);
+//   const server = app.listen(process.env.PORT || 8080, function () {
+//     const port = server.address().port;
+//     console.log("App now running on port", port);
+//   });
 // });
 
-// const server = app.listen(port, () => {
-//   const {address} = server.address();
-//   console.log(`Listening at http://${address}:${port}`);
+// const handleError = (res, reason, message, code) => {
+//   console.error(`ERROR: ${reason}`);
+//   res.status(code || 500).json({'error': message});
+// };
+
+// app.get('/api/movies', (req, res) => {
+//   db.collection(MOVIE_COLLECTION).find({}).toArray((err, documents) => {
+//     if (err) {
+//       handleError(res, err.message, "Failed to get movie data from database.");
+//     } else {
+//       res.status(200, json(documents))
+//     }
+//   });
+// });
+
+// app.post("/api/movies", function(req, res) {
+//   var newMovie = req.body;
+
+//   db.collection(MOVIE_COLLECTION).insertOne(newMovie, function(err, doc) {
+//     if (err) {
+//       handleError(res, err.message, "Failed to add new movie to database.");
+//     } else {
+//       res.status(201).json(doc.ops[0]);
+//     }
+//   });
 // });
 
 
-import express from 'express';
-import morgan from 'morgan';
-import cloudinary from 'cloudinary';
-import bodyParser from 'body-parser';
-import mongodb from 'mongodb';
+// {"title": "The Field", "year": 1990, "cloudinaryName": "thefield", "dies": true}
+// curl -H "Content-Type: application/json" -d "{'title': 'The Field', 'year': 1990, 'cloudinaryName': 'thefield', 'dies': true}" http://seanbeanapi.herokuapp.com/api/movies
 
-const {ObjectID, MongoClient} = mongodb;
-const app = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-const MOVIE_COLLECTION = "movies";
+var MOVIES_COLLECTION = "movies";
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
+var app = express();
 app.use(bodyParser.json());
 
-let db;
-MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
-  
+
   db = database;
   console.log("Database connection ready");
 
-  const server = app.listen(process.env.PORT || 8080, function () {
-    const port = server.address().port;
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
     console.log("App now running on port", port);
   });
 });
 
-const handleError = (res, reason, message, code) => {
-  console.error(`ERROR: ${reason}`);
-  res.status(code || 500).json({'error': message});
-};
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
 
-app.get('/api/movies', (req, res) => {
-  db.collection(MOVIE_COLLECTION).find({}).toArray((err, documents) => {
+
+app.get("/api/movies", function(req, res) {
+  db.collection(MOVIES_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
-      handleError(res, err.message, "Failed to get movie data from database.");
+      handleError(res, err.message, "Failed to get movies.");
     } else {
-      res.status(200, json(documents))
+      res.status(200).json(docs);
     }
   });
 });
 
 app.post("/api/movies", function(req, res) {
   var newMovie = req.body;
+  newMovie.createDate = new Date();
 
-  db.collection(MOVIE_COLLECTION).insertOne(newMovie, function(err, doc) {
+  db.collection(MOVIES_COLLECTION).insertOne(newMovie, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to add new movie to database.");
+      handleError(res, err.message, "Failed to create new movie.");
     } else {
       res.status(201).json(doc.ops[0]);
     }
@@ -95,5 +121,4 @@ app.post("/api/movies", function(req, res) {
 });
 
 
-{"title": "The Field", "year": 1990, "cloudinaryName": "thefield", "dies": true}
-curl -H "Content-Type: application/json" -d "{'title': 'The Field', 'year': 1990, 'cloudinaryName': 'thefield', 'dies': true}" http://seanbeanapi.herokuapp.com/api/movies
+// curl -H "Content-Type: application/json" -d '{"title":"The Field", "year": 1990, "cloudinaryName": "thefield", "dies": true}' http://seanbeanapi.herokuapp.com/api/contacts
