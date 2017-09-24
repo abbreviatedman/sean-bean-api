7c3735b7ecf22c56394b9b9b20850f3107eda80b
Colin Jaffe <balloonasaurus@gmail.com>
2017-09-24 14:18:44 -0400

    adds cloudinary url to records before sending to front end

--- server.js
+++ server.js
@@ -1,42 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("mongodb");
-const cloudinary = require('cloudinary');
 const ObjectID = mongodb.ObjectID;
+const cloudinary = require('cloudinary');
+require('dotenv').config()
 
 const MOVIES_COLLECTION = "movies";
 
-// cloudinary.config({ 
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-//   api_key: process.env.CLOUDINARY_API_KEY, 
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// });
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const app = express();
 app.use(bodyParser.json());
 
 let db;
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+const mongoDbUri = process.env.MONGODB_URI || 'mongodb://localhost/movies';
+
+mongodb.MongoClient.connect(mongoDbUri, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
-  // Save database object from the callback for reuse.
   db = database;
   console.log("Database connection ready");
 
-  // Initialize the app.
   const server = app.listen(process.env.PORT || 8080, function () {
     const port = server.address().port;
     console.log("App now running on port", port);
   });
 });
 
-// movies API ROUTES BELOW
-
-// Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
@@ -47,34 +45,13 @@ function addCloudinaryUrl(doc) {
   doc.url = url;
 }
 
-/*  "/api/movies"
- *    GET: finds all movies
- *    POST: creates a new contact
- */
-
 app.get("/api/movies", function(req, res) {
   db.collection(MOVIES_COLLECTION).find({}).toArray(function(err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get movies.");
     } else {
-      // transformedDocs = docs.map(addCloudinaryUrl)
+      docs.forEach(addCloudinaryUrl)
       res.status(200).json(docs);
     }
   });
 });
-
-// app.post("/api/movies", function(req, res) {
-//   var newMovie = req.body;
-
-//   db.collection(MOVIES_COLLECTION).insertOne(newMovie, function(err, doc) {
-//     if (err) {
-//       handleError(res, err.message, "Failed to create new movie.");
-//     } else {
-//       res.status(201).json(doc.ops[0]);
-//     }
-//   });
-// });
-
-
-
-// curl -H "Content-Type: application/json" -d '{"name":"mLab Support", "email": "support@mlab.com"}' http://anothermlabtest.herokuapp.com/api/movies
