e4946be47ea626b57bce08a745e1c792f71b159d
Colin Jaffe <balloonasaurus@gmail.com>
2017-11-25 18:33:00 -0500

    scale is hard

--- utilities/cloudinary.js
+++ utilities/cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary');
 
 const videoOptions = {resource_type: 'video'};
-const posterTransformOptions = {height: 20, crop: 'scale'};
+const posterTransformOptions = {height: 90, crop: 'scale'};
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
