f8a90520206f6634fe5bdaf2899c2bfcec9a55da
Colin Jaffe <balloonasaurus@gmail.com>
2017-11-25 18:50:54 -0500

    fixes minor bug from last commit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongodb.MongoClient.connect(mongoDbUri, function (err, database) {
 
 function handleError(res, reason, message, code) {
   console.log('ERROR: ' + reason);
-  res.status(code || 500).json({'g': message});
+  res.status(code || 500).json({'error': message});
 }
 
 app.get('/api/movies', function(req, res) {
