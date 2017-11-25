d604862228a20d682db60a38e2cf4fbe348af511
Colin Jaffe <balloonasaurus@gmail.com>
2017-11-25 18:42:37 -0500

    testing imdb urls

--- utilities/cloudinary.js
+++ utilities/cloudinary.js
@@ -1,7 +1,6 @@
 const cloudinary = require('cloudinary');
 
 const videoOptions = {resource_type: 'video'};
-const posterTransformOptions = {height: 100, crop: 'limit'};
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -16,11 +15,12 @@ const addVideoUrl = function(doc) {
 };
 
 const addPosterUrl = function(doc) {
-  const posterUrl = doc.posterName
-  ? cloudinary.url(doc.posterName)
-  : 'o';
+  // const posterUrl = doc.posterName
+  // ? cloudinary.url(doc.posterName)
+  // : 'o';
   
-  const urlDoc = Object.assign({}, doc, {posterUrl});
+  // const urlDoc = Object.assign({}, doc, {posterUrl});
+  const urlDoc = Object.assign({}, doc, {posterUrl: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTY0NTkxNzQxOV5BMl5BanBnXkFtZTcwNzY5MzEzMQ@@._V1_.jpg'})
   return urlDoc;
 };
 
